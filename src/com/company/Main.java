0818e513da9923ec11fc6d8133a6db65da070783
LazarVeselinov <lazar_veselinov@abv.bg>
2020-02-08 12:10:02 +0200

    Add welcome message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("zdr");
         System.out.println("hello branches");
+        System.out.println("asdasd");
     }
 }
